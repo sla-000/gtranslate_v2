845aef8b733e4a5a4f23d7e9d2a7be18d9f79d4f
Slava Ryabinin <sla.000@ya.ru>
2024-04-28 17:28:54 +0400

    fix: Add necessary exports

--- lib/gtranslate_v2.dart
+++ lib/gtranslate_v2.dart
@@ -1,3 +1,7 @@
 export 'src/gtranslate_v2.dart';
+export 'src/models/detect/detect_request_model.dart';
+export 'src/models/detect/detect_response_model.dart';
+export 'src/models/list/list_request_model.dart';
+export 'src/models/list/list_response_model.dart';
 export 'src/models/translate/translate_request_model.dart';
 export 'src/models/translate/translate_response_model.dart';
